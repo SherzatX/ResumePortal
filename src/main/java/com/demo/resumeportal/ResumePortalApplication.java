597456068a36daa364b64606233df5ff6f759460
sherzataziz <xirzat_112@hotmail.com>
2022-08-01 16:52:36 -0700

    adding user repository &JPA structure

---
+++ src/main/java/com/demo/resumeportal/ResumePortalApplication.java
@@ -0,0 +1,17 @@
+package com.demo.resumeportal;
+
+import com.demo.resumeportal.Models.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class ResumePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumePortalApplication.class, args);
+    }
+
+}
